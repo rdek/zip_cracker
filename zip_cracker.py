e89900b9efb442122be09a0f1ae1c3f64940f848
rdek <radoslawmaczuga@gmail.com>
2017-02-09 14:34:23 +0100

    Added main script

---
+++ zip_cracker.py
@@ -0,0 +1,35 @@
+import zipfile
+import sys
+import os
+from threading import Thread
+
+if len(sys.argv) < 3:
+    sys.exit('Zip password cracker, usage: %s <zipped_file_to_crack> <dictionary_path>' % sys.argv[0])
+
+if not os.path.exists(sys.argv[1]):
+    sys.exit('ERROR: ZIP file %s was not found!' % sys.argv[1])
+elif not os.path.exists(sys.argv[2]):
+    sys.exit('ERROR: %s was not found!' % sys.argv[2])
+
+def extractFile(zFile, password):
+    try:
+        zFile.extractall(pwd=password)
+        print '[+] Found password: '+password+'\n'
+    except:
+        pass
+
+def main():
+    zFile = zipfile.ZipFile(sys.argv[1])
+    passFile = open(sys.argv[2])
+
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        t = Thread(target=extractFile, args=(zFile, password))
+        t.start()
+#        guess = extractFile(zFile, password)
+#        if guess:
+#            print '[+] Password = '+password+'\n'
+#            exit(0)
+
+if __name__ == '__main__':
+    main()
